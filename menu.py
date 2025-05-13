358acbe7c76838e820d36872f7bf1405c57a815b
Krishna <kt284007@gmail.com>
2025-05-13 13:47:22 +0545

    Initial commit

--- menu.py
+++ menu.py
@@ -7,32 +7,27 @@ menu={
     "Coffee":80
 }
 #Greeting
-print("Welcome to the New Heaven Resort.")
+print("\nWelcome to the New Heaven Resort.")
 
-# for the menu to get in throw
+# for the menu to get in display
+print("\nHere is the menu for today:")
 for item,price in menu.items():
     print(f"{item}:Rs.{price}")
 
 Order_total=0
 #lets add coffee in the order
 
-item_1=input("Enter the name of the item you want to order=")
-if item_1 in menu:
-    Order_total +=menu[item_1]     #0+50
-    print(f"Your item {item_1} has been added to order.")
-else:
-    print("Sorry, we donot have this item")
+while True:
 
-Another_order= input("Do you want to add sommething else? (YES/NO)")
-if Another_order=="YES":
-    item_2=input("Enter the name of the second item=")
-    if item_2 in menu:
-        Order_total +=menu[item_2]
-        print(f"Item {item_2} has been added to your order.")
+    item=input("\nEnter the name of the item you want to order=").strip().title()
+    if item in menu:
+        Order_total +=menu[item]     #0+50
+        print(f"Your item '{item}' has been added to order.")
     else:
-        print(f"Sorry, we donot have {item_2} !")
-
+        print("Sorry, we donot have this item")
+    # lets add another item
+    #ask the user if he wants to add another item
+    Another_order = input("Do you want to add another item? (YES/NO): ").strip().upper()
+    if Another_order!="YES":
+        break
 print(f"The total amount of items to pay={Order_total}")
-
-
-
